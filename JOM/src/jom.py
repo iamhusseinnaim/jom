16e63bcf08c35616805834f477cafba6a8fb7d23
boomiraq <boomcloud177@gmail.com>
2022-04-30 23:42:14 +0300

    fix some errors

--- JOM/src/jom.py
+++ JOM/src/jom.py
@@ -1,4 +1,5 @@
 import json
+from json.decoder import JSONDecodeError
 class JsonObjectManager:
     # allowed attributes is a list contains wich  attributes you allowed to be added in this object
     # the Class will not append any other attribute that is out of this list 
@@ -7,56 +8,63 @@ class JsonObjectManager:
     allowed = []
     def __init__(self,data,allowed=[]):
         # object data -> str(Json Object) | list(Any) | dict(key&value)
-        self.data = data
+        self._data = data
         # instance controler -> dict : handled by the instance 
-        self.object = None
+        self._object = None
         # desc in the top ^ 
         self.allowed = allowed
         # instance keys 
-        self.keys = []
+        self._keys = []
         # handle data object 
-        self.handle_data()
+        self._handle_data()
+
+    def valid(self):
+        try:
+            json.loads(self.data)
+            return True
+        except JSONDecodeError:
+            return False
 
     # handle data object 
-    def handle_data(self):
-        if isinstance(self.data,str) and isinstance(json.loads(self.data),dict):
-            self.object = {}
-            keys = json.loads(self.data)
+    def _handle_data(self):
+        if isinstance(self._data,str) and isinstance(json.loads(self._data),dict):
+            self._object = {}
+            keys = json.loads(self._data)
             for key,val in keys.items():
-                self.object[key] = val
-                self.keys.append(key)
+                self._object[key] = val
+                self._keys.append(key)
 
-        elif  isinstance(self.data,dict):
-            self.object = {}
-            keys = self.data
+        elif  isinstance(self._data,dict):
+            self._object = {}
+            keys = self._data
             for key,val in keys.items():
-                self.object[key] = val
-                self.keys.append(key)
+                self._object[key] = val
+                self._keys.append(key)
 
 
-        elif isinstance(self.data,str) and isinstance(json.loads(self.data),list):
-            keys = json.loads(self.data)
-            self.object = []
+        elif isinstance(self._data,str) and isinstance(json.loads(self._data),list):
+            keys = json.loads(self._data)
+            self._object = []
             for key in keys:
-                self.object.append(key)
-                self.keys.append(key)
+                self._object.append(key)
+                self._keys.append(key)
         
-        elif isinstance(self.data,list):
-            self.object = []
-            keys = self.data
+        elif isinstance(self._data,list):
+            self._object = []
+            keys = self._data
             for key in keys:
-                self.object.append(key)
-                self.keys.append(key)
+                self._object.append(key)
+                self._keys.append(key)
 
         if not self.allowed:
-            self.allowed = self.keys
+            self.allowed = self._keys
             
         
-        if isinstance(self.object,dict):
-            for key,val in self.object.items():
+        if isinstance(self._object,dict):
+            for key,val in self._object.items():
                 setattr(self,key,val)
-        elif isinstance(self.object,list):
-            for key in self.object:
+        elif isinstance(self._object,list):
+            for key in self._object:
                 setattr(self,key,None)
 
     # check if is allowed key
@@ -65,7 +73,7 @@ class JsonObjectManager:
         
     # check if can add or change or delete  an attribute 
     def check(self,key):
-        if not self.is_allowed(key): raise AttributeError(' Object does not have a key named %s '%(key))
+        return True
     # check if the instance has a certain attribute
     def has_attr(self,key):
         try:self.check(key);return True
@@ -73,20 +81,34 @@ class JsonObjectManager:
         except:return False
     
     # change the object data 
-    def change_val(self,key,val):
-        self.check(key);self.object[key] = val ; setattr(self,key,val) ;return getattr(self,key)
+    def update(self,key,val):
+        self.check(key) 
+        if isinstance(self._object,dict):self._object[key] = val 
+        setattr(self,key,val) 
+        return getattr(self,key)
+    def set(self,key,val):
+        self.update(key,val)
+
     # delete an item from the object 
-    def del_item(self,key):
-        self.check(key) ;del self.object[key] ;delattr(self,key)
+    def delete(self,key):
+        self.check(key) 
+        if isinstance(self._object,dict):del self._object[key]
+        delattr(self,key)
     # get a certain value 
-    def get_val(self,key):
-        self.check(key) ; return getattr(self,key)
+    def get(self,key):
+        self.check(key)
+        try:return getattr(self,key)
+        except: return None
+        
+    def has_attr(self,key):
+        return hasattr(self,key)
     
     # return the fainal result for the instance -> stringfy object | dict object 
     # default is : stringfy
-    def result(self,type='json'):
-        if type != 'json':
-            return self.object
-        return json.dumps(self.object)
+    def result(self,jsonFormat=True):
+        if not jsonFormat:
+            return self._object
+        return json.dumps(self._object)
 
 JOM = JsonObjectManager
+
